a05669566e362d3de590fa74f1d72814336c32fb
Vu Hoi HUYNH <>
2024-12-16 11:20:40 +0100

    full system header: reformat

--- src/FullSystem/FullSystem.h
+++ src/FullSystem/FullSystem.h
@@ -1,51 +1,48 @@
 /**
-* This file is part of DSO.
-* 
-* Copyright 2016 Technical University of Munich and Intel.
-* Developed by Jakob Engel <engelj at in dot tum dot de>,
-* for more information see <http://vision.in.tum.de/dso>.
-* If you use this code, please cite the respective publications as
-* listed on the above website.
-*
-* DSO is free software: you can redistribute it and/or modify
-* it under the terms of the GNU General Public License as published by
-* the Free Software Foundation, either version 3 of the License, or
-* (at your option) any later version.
-*
-* DSO is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU General Public License for more details.
-*
-* You should have received a copy of the GNU General Public License
-* along with DSO. If not, see <http://www.gnu.org/licenses/>.
-*/
-
+ * This file is part of DSO.
+ *
+ * Copyright 2016 Technical University of Munich and Intel.
+ * Developed by Jakob Engel <engelj at in dot tum dot de>,
+ * for more information see <http://vision.in.tum.de/dso>.
+ * If you use this code, please cite the respective publications as
+ * listed on the above website.
+ *
+ * DSO is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * DSO is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with DSO. If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #pragma once
+
 #define MAX_ACTIVE_FRAMES 100
 
+#include <math.h>
 #include <deque>
-#include "util/NumType.h"
-#include "util/globalCalib.h"
-#include "vector"
- 
-#include <iostream>
 #include <fstream>
-#include "util/NumType.h"
-#include "FullSystem/Residuals.h"
+#include <iostream>
+#include "vector"
+
 #include "FullSystem/HessianBlocks.h"
+#include "FullSystem/PixelSelector2.h"
+#include "FullSystem/Residuals.h"
+#include "OptimizationBackend/EnergyFunctional.h"
 #include "util/FrameShell.h"
 #include "util/IndexThreadReduce.h"
-#include "OptimizationBackend/EnergyFunctional.h"
-#include "FullSystem/PixelSelector2.h"
+#include "util/NumType.h"
+#include "util/globalCalib.h"
 
-#include <math.h>
+namespace dso {
 
-namespace dso
-{
-namespace IOWrap
-{
+namespace IOWrap {
 class Output3DWrapper;
 } // namespace IOWrap
 
@@ -58,256 +55,268 @@ class CoarseInitializer;
 struct ImmaturePointTemporaryResidual;
 class ImageAndExposure;
 class CoarseDistanceMap;
-
 class EnergyFunctional;
 
 // Delete the i-th element from a vector and move the last element to its place.
-template<typename T> inline void deleteOut(std::vector<T*> &v, const int i)
-{
-	delete v[i];
-	v[i] = v.back();
-	v.pop_back();
+template <typename T>
+inline void deleteOut(std::vector<T*>& v, const int i) {
+  delete v[i];
+  v[i] = v.back();
+  v.pop_back();
 }
 // Delete the element from a vector and move the last element to its place.
-template<typename T> inline void deleteOutPt(std::vector<T*> &v, const T* i)
-{
-	delete i;
+template <typename T>
+inline void deleteOutPt(std::vector<T*>& v, const T* e) {
+  delete e;
 
-	for(unsigned int k=0;k<v.size();k++)
-		if(v[k] == i)
-		{
-			v[k] = v.back();
-			v.pop_back();
-		}
+  for (unsigned int i = 0; i < v.size(); i++) {
+    if (v[i] == e) {
+      v[i] = v.back();
+      v.pop_back();
+    }
+  }
 }
 // Delete the i-th element from a vector and increment all following elements by
 // one position.
-template<typename T> inline void deleteOutOrder(std::vector<T*> &v, const int i)
-{
-	delete v[i];
-	for(unsigned int k=i+1; k<v.size();k++)
-		v[k-1] = v[k];
-	v.pop_back();
+template <typename T>
+inline void deleteOutOrder(std::vector<T*>& v, const int i) {
+  delete v[i];
+  for (unsigned int k = i + 1; k < v.size(); k++) {
+    v[k - 1] = v[k];
+  }
+  v.pop_back();
 }
 // Delete the element from a vector and increment all following elements by one
 // position.
-template<typename T> inline void deleteOutOrder(std::vector<T*> &v, const T* element)
-{
-	int i=-1;
-	for(unsigned int k=0; k<v.size();k++)
-	{
-		if(v[k] == element)
-		{
-			i=k;
-			break;
-		}
-	}
-	assert(i!=-1);
+template <typename T>
+inline void deleteOutOrder(std::vector<T*>& v, const T* e) {
+  int i = -1;
+  for (unsigned int k = 0; k < v.size(); k++) {
+    if (v[k] == e) {
+      i = k;
+      break;
+    }
+  }
+  assert(i != -1);
 
-	for(unsigned int k=i+1; k<v.size();k++)
-		v[k-1] = v[k];
-	v.pop_back();
+  for (unsigned int k = i + 1; k < v.size(); k++) {
+    v[k - 1] = v[k];
+  }
+  v.pop_back();
 
-	delete element;
+  delete e;
 }
 // Check if a matrix contains NaN values.
-inline bool eigenTestNan(const MatXX &m, std::string msg)
-{
-	bool foundNan = false;
-	for(int y=0;y<m.rows();y++)
-		for(int x=0;x<m.cols();x++)
-		{
-			if(!std::isfinite((double)m(y,x))) foundNan = true;
-		}
+inline bool eigenTestNan(const MatXX& m, std::string msg) {
+  bool foundNan = false;
+  for (int y = 0; y < m.rows(); y++) {
+    for (int x = 0; x < m.cols(); x++) {
+      if (!std::isfinite((double)m(y, x))) {
+        foundNan = true;
+      }
+    }
+  }
 
-	if(foundNan)
-	{
-		printf("NAN in %s:\n",msg.c_str());
-		std::cout << m << "\n\n";
-	}
+  if (foundNan) {
+    printf("NAN in %s:\n", msg.c_str());
+    std::cout << m << "\n\n";
+  }
 
-
-	return foundNan;
+  return foundNan;
 }
-
-
-
-
 
 class FullSystem {
 public:
-	EIGEN_MAKE_ALIGNED_OPERATOR_NEW
-	FullSystem();
-	virtual ~FullSystem();
+  EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+  FullSystem();
+  virtual ~FullSystem();
 
   // Add a new frame and creates point & residual structs.
-	void addActiveFrame(ImageAndExposure* image, int id);
+  void addActiveFrame(ImageAndExposure* image, int id);
 
   // Marginalize a frame. Drop/Marginalize points & residuals.
-	void marginalizeFrame(FrameHessian* frame);
-	void blockUntilMappingIsFinished();
+  void marginalizeFrame(FrameHessian* frame);
 
-	float optimize(int mnumOptIts);
+  void blockUntilMappingIsFinished();
 
-	void printResult(std::string file);
+  float optimize(int mnumOptIts);
 
-	void debugPlot(std::string name);
+  void printResult(std::string file);
 
-	void printFrameLifetimes();
+  void debugPlot(std::string name);
 
-    std::vector<IOWrap::Output3DWrapper*> outputWrapper;
+  void printFrameLifetimes();
 
-	bool isLost;
-	bool initFailed;
-	bool initialized;
-	bool linearizeOperation;
+  std::vector<IOWrap::Output3DWrapper*> outputWrapper;
 
+  bool isLost;
+  bool initFailed;
+  bool initialized;
+  bool linearizeOperation;
 
-	void setGammaFunction(float* BInv);
-	void setOriginalCalib(const VecXf &originalCalib, int originalW, int originalH);
+  void setGammaFunction(float* BInv);
+
+  void setOriginalCalib(const VecXf& originalCalib, int originalW, int originalH);
 
 private:
-
-	CalibHessian Hcalib;
-
-
-
+  CalibHessian Hcalib;
 
   // Optimize single point.
-	int optimizePoint(PointHessian* point, int minObs, bool flagOOB);
-	PointHessian* optimizeImmaturePoint(ImmaturePoint* point, int minObs, ImmaturePointTemporaryResidual* residuals);
+  int optimizePoint(PointHessian* point, int minObs, bool flagOOB);
+  PointHessian* optimizeImmaturePoint(
+    ImmaturePoint* point,
+    int minObs,
+    ImmaturePointTemporaryResidual* residuals
+  );
 
-	double linAllPointSinle(PointHessian* point, float outlierTHSlack, bool plot);
+  double linAllPointSinle(PointHessian* point, float outlierTHSlack, bool plot);
 
   // Main pipeline functions.
-	Vec4 trackNewCoarse(FrameHessian* fh);
-	void traceNewCoarse(FrameHessian* fh);
-	void activatePoints();
-	void activatePointsMT();
-	void activatePointsOldFirst();
-	void flagPointsForRemoval();
-	void makeNewTraces(FrameHessian* newFrame, float* gtDepth);
-	void initializeFromInitializer(FrameHessian* newFrame);
-	void flagFramesForMarginalization(FrameHessian* newFH);
+  Vec4 trackNewCoarse(FrameHessian* fh);
+  void traceNewCoarse(FrameHessian* fh);
+  void activatePoints();
+  void activatePointsMT();
+  void activatePointsOldFirst();
+  void flagPointsForRemoval();
+  void makeNewTraces(FrameHessian* newFrame, float* gtDepth);
+  void initializeFromInitializer(FrameHessian* newFrame);
+  void flagFramesForMarginalization(FrameHessian* newFH);
 
+  void removeOutliers();
 
-	void removeOutliers();
-
-
-	void setPrecalcValues();
+  void setPrecalcValues();
 
   // Solve the system (Eventually migrate to EnergyFunctional).
-	void solveSystem(int iteration, double lambda);
-	Vec3 linearizeAll(bool fixLinearization);
-	bool doStepFromBackup(float stepfacC,float stepfacT,float stepfacR,float stepfacA,float stepfacD);
-	void backupState(bool backupLastStep);
-	void loadSateBackup();
-	double calcLEnergy();
-	double calcMEnergy();
-	void linearizeAll_Reductor(bool fixLinearization, std::vector<PointFrameResidual*>* toRemove, int min, int max, Vec10* stats, int tid);
-	void activatePointsMT_Reductor(std::vector<PointHessian*>* optimized,std::vector<ImmaturePoint*>* toOptimize,int min, int max, Vec10* stats, int tid);
-	void applyRes_Reductor(bool copyJacobians, int min, int max, Vec10* stats, int tid);
+  void solveSystem(int iteration, double lambda);
+  Vec3 linearizeAll(bool fixLinearization);
+  bool doStepFromBackup(
+    float stepfacC,
+    float stepfacT,
+    float stepfacR,
+    float stepfacA,
+    float stepfacD
+  );
+  void backupState(bool backupLastStep);
+  void loadSateBackup();
+  double calcLEnergy();
+  double calcMEnergy();
+  void linearizeAll_Reductor(
+    bool fixLinearization,
+    std::vector<PointFrameResidual*>* toRemove,
+    int min,
+    int max,
+    Vec10* stats,
+    int tid
+  );
+  void activatePointsMT_Reductor(
+    std::vector<PointHessian*>* optimized,
+    std::vector<ImmaturePoint*>* toOptimize,
+    int min,
+    int max,
+    Vec10* stats,
+    int tid
+  );
+  void applyRes_Reductor(bool copyJacobians, int min, int max, Vec10* stats, int tid);
 
-	void printOptRes(const Vec3 &res, double resL, double resM, double resPrior, double LExact, float a, float b);
+  void printOptRes(
+    const Vec3& res,
+    double resL,
+    double resM,
+    double resPrior,
+    double LExact,
+    float a,
+    float b
+  );
 
-	void debugPlotTracking();
+  void debugPlotTracking();
 
-	std::vector<VecX> getNullspaces(
-			std::vector<VecX> &nullspaces_pose,
-			std::vector<VecX> &nullspaces_scale,
-			std::vector<VecX> &nullspaces_affA,
-			std::vector<VecX> &nullspaces_affB);
+  std::vector<VecX> getNullspaces(
+    std::vector<VecX>& nullspaces_pose,
+    std::vector<VecX>& nullspaces_scale,
+    std::vector<VecX>& nullspaces_affA,
+    std::vector<VecX>& nullspaces_affB
+  );
 
-	void setNewFrameEnergyTH();
+  void setNewFrameEnergyTH();
 
+  void printLogLine();
+  void printEvalLine();
+  void printEigenValLine();
+  std::ofstream* calibLog;
+  std::ofstream* numsLog;
+  std::ofstream* errorsLog;
+  std::ofstream* eigenAllLog;
+  std::ofstream* eigenPLog;
+  std::ofstream* eigenALog;
+  std::ofstream* DiagonalLog;
+  std::ofstream* variancesLog;
+  std::ofstream* nullspacesLog;
 
-	void printLogLine();
-	void printEvalLine();
-	void printEigenValLine();
-	std::ofstream* calibLog;
-	std::ofstream* numsLog;
-	std::ofstream* errorsLog;
-	std::ofstream* eigenAllLog;
-	std::ofstream* eigenPLog;
-	std::ofstream* eigenALog;
-	std::ofstream* DiagonalLog;
-	std::ofstream* variancesLog;
-	std::ofstream* nullspacesLog;
-
-	std::ofstream* coarseTrackingLog;
+  std::ofstream* coarseTrackingLog;
 
   // Statistics.
-	long int statistics_lastNumOptIts;
-	long int statistics_numDroppedPoints;
-	long int statistics_numActivatedPoints;
-	long int statistics_numCreatedPoints;
-	long int statistics_numForceDroppedResBwd;
-	long int statistics_numForceDroppedResFwd;
-	long int statistics_numMargResFwd;
-	long int statistics_numMargResBwd;
-	float statistics_lastFineTrackRMSE;
-
-
-
-
-
+  long int statistics_lastNumOptIts;
+  long int statistics_numDroppedPoints;
+  long int statistics_numActivatedPoints;
+  long int statistics_numCreatedPoints;
+  long int statistics_numForceDroppedResBwd;
+  long int statistics_numForceDroppedResFwd;
+  long int statistics_numMargResFwd;
+  long int statistics_numMargResBwd;
+  float statistics_lastFineTrackRMSE;
 
   // Variables changed by tracker thread, protected by trackMutex.
-	boost::mutex trackMutex;
-	std::vector<FrameShell*> allFrameHistory;
-	CoarseInitializer* coarseInitializer;
-	Vec5 lastCoarseRMSE;
+  boost::mutex trackMutex;
+  std::vector<FrameShell*> allFrameHistory;
+  CoarseInitializer* coarseInitializer;
+  Vec5 lastCoarseRMSE;
 
   // Variables changed by mapping thread, protected by mapMutex.
-	boost::mutex mapMutex;
-	std::vector<FrameShell*> allKeyFramesHistory;
+  boost::mutex mapMutex;
+  std::vector<FrameShell*> allKeyFramesHistory;
 
-	EnergyFunctional* ef;
-	IndexThreadReduce<Vec10> treadReduce;
+  EnergyFunctional* ef;
+  IndexThreadReduce<Vec10> treadReduce;
 
-	float* selectionMap;
-	PixelSelector* pixelSelector;
-	CoarseDistanceMap* coarseDistanceMap;
+  float* selectionMap;
+  PixelSelector* pixelSelector;
+  CoarseDistanceMap* coarseDistanceMap;
 
-	std::vector<FrameHessian*> frameHessians; // ONLY changed in marginalizeFrame and addFrame.
-	std::vector<PointFrameResidual*> activeResiduals;
-	float currentMinActDist;
+  std::vector<FrameHessian*> frameHessians; // ONLY changed in marginalizeFrame and addFrame.
+  std::vector<PointFrameResidual*> activeResiduals;
+  float currentMinActDist;
 
-
-	std::vector<float> allResVec;
-
+  std::vector<float> allResVec;
 
   // Variables for tracker exchange, protected by [coarseTrackerSwapMutex].
-	boost::mutex coarseTrackerSwapMutex; // If tracker sees that there is a new reference, tracker locks [coarseTrackerSwapMutex] and swaps the two.
-	CoarseTracker* coarseTracker_forNewKF; // Set as as reference. protected by [coarseTrackerSwapMutex].
-	CoarseTracker* coarseTracker; // Always used to track new frames. protected by [trackMutex].
-	float minIdJetVisTracker, maxIdJetVisTracker;
-	float minIdJetVisDebug, maxIdJetVisDebug;
-
-
-
+  boost::mutex coarseTrackerSwapMutex; // If tracker sees that there is a new reference, tracker
+                                       // locks [coarseTrackerSwapMutex] and swaps the two.
+  CoarseTracker* coarseTracker_forNewKF; // Set as as reference. protected by
+                                         // [coarseTrackerSwapMutex].
+  CoarseTracker* coarseTracker; // Always used to track new frames. protected by [trackMutex].
+  float minIdJetVisTracker, maxIdJetVisTracker;
+  float minIdJetVisDebug, maxIdJetVisDebug;
 
   // Mutex for camToWorld's in shells (these are always in a good configuration).
-	boost::mutex shellPoseMutex;
-
-
+  boost::mutex shellPoseMutex;
 
   // Tracking always uses the newest KF as reference.
-	void makeKeyFrame( FrameHessian* fh);
-	void makeNonKeyFrame( FrameHessian* fh);
-	void deliverTrackedFrame(FrameHessian* fh, bool needKF);
-	void mappingLoop();
+  void makeKeyFrame(FrameHessian* fh);
+  void makeNonKeyFrame(FrameHessian* fh);
+  void deliverTrackedFrame(FrameHessian* fh, bool needKF);
+  void mappingLoop();
 
   // Tracking / mapping synchronization. All protected by [trackMapSyncMutex].
-	boost::mutex trackMapSyncMutex;
-	boost::condition_variable trackedFrameSignal;
-	boost::condition_variable mappedFrameSignal;
-	std::deque<FrameHessian*> unmappedTrackedFrames;
-	int needNewKFAfter;	// Otherwise, a new KF is needed that has ID bigger than [needNewKFAfter].
-	boost::thread mappingThread;
-	bool runMapping;
-	bool needToKetchupMapping;
+  boost::mutex trackMapSyncMutex;
+  boost::condition_variable trackedFrameSignal;
+  boost::condition_variable mappedFrameSignal;
+  std::deque<FrameHessian*> unmappedTrackedFrames;
+  int needNewKFAfter; // Otherwise, a new KF is needed that has ID bigger than [needNewKFAfter].
+  boost::thread mappingThread;
+  bool runMapping;
+  bool needToKetchupMapping;
 
-	int lastRefStopID;
+  int lastRefStopID;
 };
+
 } // namespace dso
